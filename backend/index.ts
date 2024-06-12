ab7c4872d6cb7b6026ca7cc21e63cc879d010493
FilledEther <gairolachaitanya@gmail.com>
2024-06-12 23:36:29 +0530

    backend related functionaliteis

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+import express,{Request,Response} from 'express';
 const app = express();
 app.use(express.json());
 
 const PORT = 3003;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req: Request, res: Response) => {
   console.log('someone pinged here');
   res.send('pong');
 });
 
-app.listen(PORT, () => {
+app.listen((PORT), () => {
   console.log(`Server running on port ${PORT}`);
 });
