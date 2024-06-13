98d144b209213e7068c797717fbdd2c0af9cde74
FilledEther <gairolachaitanya@gmail.com>
2024-06-13 20:47:08 +0530

    Route for diagnoses defined

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 import patientatorRouter from './routes/routes';
 app.use(express.json());
-
+app.use(cors());
 const PORT = 3003;
 
 //route for diagnoses
