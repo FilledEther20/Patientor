b2f0fdbbad4c87efa2bd03df9186f4686dfe9d7e
FilledEther <gairolachaitanya@gmail.com>
2024-06-13 11:03:54 +0530

    api route for diagnoses implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,12 @@
-import express,{Request,Response} from 'express';
+import express from 'express';
 const app = express();
+import patientatorRouter from './routes/routes';
 app.use(express.json());
 
 const PORT = 3003;
 
-app.get('/api/ping', (_req: Request, res: Response) => {
-  console.log('someone pinged here');
-  res.send('pong');
-});
-
-app.listen((PORT), () => {
-  console.log(`Server running on port ${PORT}`);
+//route for diagnoses
+app.use('/api/diagnoses', patientatorRouter);
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
