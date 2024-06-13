61194e81e37424b04973294c31d2e50963c00a38
FilledEther <gairolachaitanya@gmail.com>
2024-06-13 11:05:28 +0530

    routes for diagnoses data implemented

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,8 +1,11 @@
-import express from 'express';
-import diagnosesData from '../../../data/diagnoses';
-import patientData from '../../../data/patient';
+import express, { Request, Response } from 'express';
+import diagnosesData from '../data/diagnoses';
+import patientData from '../data/patient';
 import { Diagnoses } from '../types';
-const router=express.Router();
-router.get('/api/diagnoses',(_req,res)=>{
-    res.send(diagnosesData)
-})
+const router = express.Router();
+
+router.get('/', (_req: Request, res: Response) => {
+	res.send(diagnosesData);
+});
+
+export default router;
