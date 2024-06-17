cde34a82c7710838bf02b51a020743a1c6295ddb
FilledEther <gairolachaitanya@gmail.com>
2024-06-17 19:52:11 +0530

    type narrowing through the use of enum implemented

--- backend/src/models/patient_model.ts
+++ backend/src/models/patient_model.ts
@@ -3,8 +3,14 @@ export interface Patient{
     name:string,
     dateOfBirth:string,
     ssn:string,
-    gender:string,
+    gender:'male'|'female'|'other',
     occupation:string,
 }
 
 export type SafePatientData=Omit<Patient,'ssn'>;
+
+export enum Gender{
+    Male='male',
+    Female='female',
+    Other='other',
+}
