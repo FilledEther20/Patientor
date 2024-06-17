380e437a796b78e80b444f1a831909c469fe7909
FilledEther <gairolachaitanya@gmail.com>
2024-06-17 22:37:33 +0530

    Updated the url for router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-import patientatorRouter from './routes/routes';
+import patientatorRouter from './routes/patient';
 app.use(express.json());
 app.use(cors());
 const PORT = 3003;
 
 //route for diagnoses
-app.use('/api', patientatorRouter);
+app.use('/api/patient', patientatorRouter);
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
