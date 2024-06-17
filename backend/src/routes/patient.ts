308a4fd86332a542ac4571f12456ef90c1da2596
FilledEther <gairolachaitanya@gmail.com>
2024-06-17 22:02:35 +0530

    Post route implemented for entries

--- backend/src/routes/patient.ts
+++ backend/src/routes/patient.ts
@@ -1,6 +1,6 @@
 import  express,{Request,Response}  from "express";
 import patientorService from '../services/services';
-
+import toNewPatientEntry from '../utils';
 const patientRouter = express.Router();
 
 patientRouter.get('/patient', (_req: Request, res: Response) => {
@@ -8,6 +8,18 @@ patientRouter.get('/patient', (_req: Request, res: Response) => {
 	res.send(data);
 });
 
-patientRouter.post('/patient', (req, res) => {});
+patientRouter.get('/:patientId',(req:Request,res:Response)=>{
+    res.send(patientorService.getPatientbyId(req.params.patientId));
+})
+
+patientRouter.post('/patient', (req:Request, res:Response) => {
+    try {
+        const newPatientEntry=toNewPatientEntry(req.body);
+        const addedEntry=patientorService.addPatient(newPatientEntry);
+        res.json(addedEntry);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
 
 export default patientRouter;
