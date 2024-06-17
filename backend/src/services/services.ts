144c9c48fe059be3c47df3d98e27ec1d86db453c
FilledEther <gairolachaitanya@gmail.com>
2024-06-17 22:01:14 +0530

    add patient functionality added for post route

--- backend/src/services/services.ts
+++ backend/src/services/services.ts
@@ -1,6 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
 import diagnosesData from '../data/diagnoses';
 import patientData from '../data/patient';
-import { SafePatientData } from '../models/patient_model';
+import { SafePatientData,Patient} from '../models/patient_model';
 import { Diagnoses } from '../models/diagnose_model';
 
 const getDiagnoses=():Diagnoses[]=>{
@@ -21,8 +22,18 @@ const getPatientbyId=(id:string):SafePatientData|undefined=>{
 	return patientData.find((patient)=>patient.id==id);
 }
 
+const addPatient=(entry:SafePatientData):Patient=>{
+	const newPatientEntry = {
+		id: uuidv4(),
+		...entry
+	  };
+	patientData.push(newPatientEntry);
+	return newPatientEntry;
+}
+
 export default {
     getDiagnoses,
 	getSafePatientData,
 	getPatientbyId,
+	addPatient,
 };
