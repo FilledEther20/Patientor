9afd8024dd371893b8c5a676484aa308c57f0663
FilledEther <gairolachaitanya@gmail.com>
2024-06-19 23:02:39 +0530

    Stricter type checks implemneted

--- backend/src/services/services.ts
+++ backend/src/services/services.ts
@@ -1,39 +1,41 @@
 import { v4 as uuidv4 } from 'uuid';
 import diagnosesData from '../data/diagnoses';
 import patientData from '../data/patient';
-import { SafePatientData,Patient} from '../models/patient_model';
+import { SafePatientData, Patient } from '../models/patient_model';
 import { Diagnoses } from '../models/diagnose_model';
 
-const getDiagnoses=():Diagnoses[]=>{
-    return diagnosesData
-}
-
-const getSafePatientData = (): SafePatientData[] => {
-	return patientData.map(({ ssn, name, dateOfBirth, gender, occupation }) => ({
-		ssn,
-		name,
-		dateOfBirth,
-		gender,
-		occupation,
-	}));
+const getDiagnoses = (): Diagnoses[] => {
+    return diagnosesData;
 };
 
-const getPatientbyId=(id:string):SafePatientData|undefined=>{
-	return patientData.find((patient)=>patient.id==id);
-}
+const getSafePatientData = (): SafePatientData[] => {
+    return patientData.map(({ ssn, name, dateOfBirth, gender, occupation }) => ({
+        ssn,
+        name,
+        dateOfBirth,
+        gender,
+        occupation,
+    }));
+};
 
-const addPatient=(entry:SafePatientData):Patient=>{
-	const newPatientEntry = {
-		id: uuidv4(),
-		...entry
-	  };
-	patientData.push(newPatientEntry);
-	return newPatientEntry;
-}
+const getPatientbyId = (id: string): SafePatientData | undefined => {
+    const patient = patientData.find((patient) => patient.id === id);
+    return patient;
+};
+
+const addPatient = (entry: SafePatientData): Patient => {
+    const newPatientEntry: Patient = {
+        id: uuidv4(),
+		entries:[],
+        ...entry,
+    };
+    patientData.push(newPatientEntry);
+    return newPatientEntry;
+};
 
 export default {
     getDiagnoses,
-	getSafePatientData,
-	getPatientbyId,
-	addPatient,
+    getSafePatientData,
+    getPatientbyId,
+    addPatient,
 };
