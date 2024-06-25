701aed31d93fd76f0e06141301da53a150e729f6
FilledEther <gairolachaitanya@gmail.com>
2024-06-26 03:02:18 +0530

    Diagnoses route implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-import patientatorRouter from './routes/patient';
+import patientatorRouter from './routes/route_pat';
+import diagnosesRouter from './routes/route_dia';
 app.use(express.json());
 app.use(cors());
 const PORT = 3003;
 
-//route for diagnoses
+//route for patients
 app.use('/api/patient', patientatorRouter);
+
+app.use('/api/diagnoses',diagnosesRouter);
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
