81c09ea3223b96accb2ae4c0401e62b1790ce017
FilledEther <gairolachaitanya@gmail.com>
2024-06-26 14:52:41 +0530

    BaseEntry along with HealthCheckEntry defined

--- backend/src/models/diagnose_model.ts
+++ backend/src/models/diagnose_model.ts
@@ -3,3 +3,21 @@ export interface Diagnoses{
     name:string,
     latin?:string   
 }
+
+interface BaseEntry{
+    id:string,
+    date:string,
+    type:string,
+    specialist:string,
+    diagnosisCodes:Array<Diagnoses['code']>,
+}
+export enum HealthCheckRating{
+    "Healthy"=0,
+    "LowRisk"=1,
+    "HighRisk"=2,
+    "CriticalRisk"=3,
+}
+interface HealthCheckEntry extends BaseEntry{
+    type:"HealthCheck";
+    healthCheckRating:HealthCheckRating;
+}
